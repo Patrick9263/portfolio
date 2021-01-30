0ef92ef6f245a1306bb2441eb2b37cd49b253189
patrick9263 <psmith9263@gmail.com>
2021-01-29 21:26:25 -0500

    work on parralax effect

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,56 +1,59 @@
 import React from 'react'
-import { ParallaxBanner } from 'react-scroll-parallax'
+// import { ParallaxBanner } from 'react-scroll-parallax'
+import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons'
+
 import Navigation from '../Navigation/Navigation'
-import './style.scss'
+import './Home.scss'
 
 import profilePic from '../../../assets/profilePic.png'
 import banner from '../../../assets/banner.jpeg'
+// other background
+// https://wallpaper.dog/large/5558065.jpg
+// https://www.wallpapertip.com/wmimgs/116-1168442_landscape-4k-mountains.jpg
 
 // https://github.com/jscottsmith/react-scroll-parallax#banner-layers-prop
 
 const Home = () => {
+	const bannerStyle = {
+		backgroundImage: `url(${banner})`
+	}
+	const secondPageStyle = {
+		backgroundImage: `url(https://wallpaper.dog/large/5558065.jpg)`,
+		backgroundSize: 'cover'
+	}
 	return (
-		<div className='flex flex-col justify-center items-center select-none min-h-screen bg-gradient-to-br from-gray-900 to-blue-700'>
+		<>
 			<Navigation />
 
-			<img
-				className='w-50 h-50 mx-auto'
-				src={banner}
-				// src='https://www.wallpapertip.com/wmimgs/116-1168442_landscape-4k-mountains.jpg'
-				alt=''
-				width='100%'
-				height='100vh'
-			/>
+			<Parallax pages={2} scrolling horizontal={false}>
+				<ParallaxLayer factor={1} offset={0} speed={0.1}>
+					<div className='background-image' style={bannerStyle} />
+					<div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+						<img
+							className='w-50 h-50 rounded-full mx-auto'
+							style={{ position: 'absolute', top: '55vh' }}
+							src={profilePic}
+							alt=''
+							width='384'
+							height='512'
+						/>
+						<h1 className='text-6xl text-white border-b-4 pb-4' style={{ position: 'absolute', top: '85vh' }}>
+							Patrick Smith
+						</h1>
+					</div>
+				</ParallaxLayer>
 
-			<img
-				className='w-50 h-50 rounded-full mx-auto'
-				style={{ position: 'absolute', top: '55vh' }}
-				src={profilePic}
-				alt=''
-				width='384'
-				height='512'
-			/>
-
-			<h1 className='text-6xl text-white border-b-4 pb-4' style={{ position: 'absolute', top: '85vh' }}>
-				Patrick Smith
-			</h1>
-
-			<ParallaxBanner
-				id='about'
-				className=''
-				layers={[
-					{
-						image: 'https://wallpaper.dog/large/5558065.jpg',
-						amount: 0.1
-					}
-				]}
-				style={{
-					filter: 'blur(8px)',
-					WebkitFilter: 'blur(8px)',
-					opacity: 0.2
-				}}
-			/>
-		</div>
+				<ParallaxLayer factor={1} offset={1} speed={0.1}>
+					<div className='background-image' style={secondPageStyle}>
+						<div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+							<h1 id='about' className='element'>
+								Scroll to element
+							</h1>
+						</div>
+					</div>
+				</ParallaxLayer>
+			</Parallax>
+		</>
 	)
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
+import { Link } from 'react-scroll'
 import './Navigation.scss'
 
 const Navigation = () => {
-	const linkStyles = 'text-2 text-gray-300 mr-5 shadow-sm navLinks'
+	const linkStyles = 'navLinks text-2 text-gray-300 mr-10 shadow-sm'
 
 	return (
-		<div className='flex flex-row justify-end items-center select-none absolute inset-x-0 top-0 h-16'>
-			<a href='#about' className={linkStyles}>
+		<div className='navBar flex flex-row justify-end items-center select-none absolute inset-x-0 top-0 h-16'>
+			<Link className={linkStyles} to='about' spy={false} smooth duration={500}>
 				About
-			</a>
+			</Link>
 
 			<a href='#career' className={linkStyles}>
 				Career
@@ -26,28 +27,6 @@ const Navigation = () => {
 			<a href='#resume' className={linkStyles}>
 				Resume
 			</a>
-
-			{/*
-			<Link to='#about' className={linkStyles}>
-				About
-			</Link>
-
-			<Link to='#career' className={linkStyles}>
-				Career
-			</Link>
-
-			<Link to='#education' className={linkStyles}>
-				Education
-			</Link>
-
-			<Link to='#projects' className={linkStyles}>
-				Projects
-			</Link>
-
-			<Link to='#resume' className={linkStyles}>
-				Resume
-			</Link>
-*/}
 		</div>
 	)
 }
